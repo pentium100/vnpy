dd641bd4a3cabed720897bb64a16bf45fefe912e
Liwei Chen <liwei77@hotmail.com>
2017-07-05 10:30:27 +0800

    send sms bug fix.

---
+++ vnpy/trader/app/ctaStrategy/strategy/strategySpread4.py
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+"""
+差价交易计算三个合约当前可成交价格的最新差价。
+ 只做测试用：前两个合约的卖1相加-最后一个合约的买1价
+"""
+
+
+from strategySpread import SpreadStrategy
+
+
+########################################################################
+class SpreadStrategy4(SpreadStrategy):
+    """三合约价差计算Demo"""
+    className = 'SpreadCalcDemo4'
+    author = u'clw@itg'
+
+
+    # ----------------------------------------------------------------------
+    def __init__(self, ctaEngine, setting):
+        """Constructor"""
+        super(SpreadStrategy4, self).__init__(ctaEngine, setting)
+
+
+
